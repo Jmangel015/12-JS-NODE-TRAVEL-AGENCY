37167cdccd2ee3f9532dfeb14da804d7aba6e448
Jorge Miguel Angel <jangel@people-inmotion.com>
2020-11-22 20:36:16 -0500

    se agregaros las vistas y el template engine

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ const port = process.env.PORT || 4000;
 
 //HAbilitar PUG
 app.set('view engine', 'pug');
+//Obtener el año actual
+app.use((req, res, next) => {
+  const year = new Date();
+  res.locals.ActualYear = year.getFullYear();
+  res.locals.nombreSitio = 'Agencia De Viajes';
+  return next();
+});
+//Definir la carpeta publica
+app.use(express.static('public'));
 //Agregar Router
 app.use('/', router);
 
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,23 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   // req - lo que enviamos : res - lo que expressnos responde
-  res.render('inicio');
-});
-router.get('/nosotros', (req, res) => {
-  const viajes = 'Viajes a Alemania';
-  res.render('nosotros', {
-    viajes,
+  res.render('inicio', {
+    pagina: 'Inicio',
   });
 });
-
+router.get('/nosotros', (req, res) => {
+  res.render('nosotros', {
+    pagina: 'Nosotros',
+  });
+});
+router.get('/viajes', (req, res) => {
+  res.render('viajes', {
+    pagina: 'Viajes',
+  });
+});
+router.get('/testimoniales', (req, res) => {
+  res.render('testimoniales', {
+    pagina: 'Testimoniales',
+  });
+});
 export default router;
