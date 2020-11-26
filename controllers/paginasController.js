c46434232213c79ccafda9cbf54c0057d590b7bc
Jorge Miguel Angel <jangel@people-inmotion.com>
2020-11-26 17:48:45 -0500

    Se agrego MVC, Sequalize, ConexionBD

---
+++ controllers/paginasController.js
@@ -0,0 +1,22 @@
+const paginaInicio = (req, res) => {
+  res.render('inicio', {
+    pagina: 'Inicio',
+  });
+};
+
+const paginaNosotros = (req, res) => {
+  res.render('nosotros', {
+    pagina: 'Nosotros',
+  });
+};
+const paginaViajes = (req, res) => {
+  res.render('viajes', {
+    pagina: 'Viajes',
+  });
+};
+const paginaTestimonios = (req, res) => {
+  res.render('testimoniales', {
+    pagina: 'Testimoniales',
+  });
+};
+export { paginaInicio, paginaNosotros, paginaViajes, paginaTestimonios };
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,17 @@
 import express from 'express';
+import {
+  paginaInicio,
+  paginaNosotros,
+  paginaViajes,
+  paginaTestimonios,
+} from '../controllers/paginasController.js';
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // req - lo que enviamos : res - lo que expressnos responde
-  res.render('inicio', {
-    pagina: 'Inicio',
-  });
-});
-router.get('/nosotros', (req, res) => {
-  res.render('nosotros', {
-    pagina: 'Nosotros',
-  });
-});
-router.get('/viajes', (req, res) => {
-  res.render('viajes', {
-    pagina: 'Viajes',
-  });
-});
-router.get('/testimoniales', (req, res) => {
-  res.render('testimoniales', {
-    pagina: 'Testimoniales',
-  });
-});
+router.get('/', paginaInicio);
+// req - lo que enviamos : res - lo que expressnos responde
+
+router.get('/nosotros', paginaNosotros);
+router.get('/viajes', paginaViajes);
+router.get('/testimoniales', paginaTestimonios);
 export default router;
